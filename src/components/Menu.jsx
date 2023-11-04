fca557bf8268703323325b8c6c5b7c5af1873b1d
Mafe sanchez <maria.sanchez02@uptc.edu.co>
2023-11-04 14:37:01 -0500

    update workshop

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -15,21 +15,34 @@ import WorkshopList from "./workshopList";
 const Menu = ({ handleLogout }) => {
   function LogOut() {
     handleLogout();
-    document.getElementById("caja_menu").style.display = "none";
-    document.getElementById("form_login").style.display = "block";
-    document.getElementById("txtusu").value = " ";
-    document.getElementById("txtusu").focus();
+    const cajaMenu = document.getElementById("caja_menu");
+    const formLogin = document.getElementById("form_login");
+    const txtUsu = document.getElementById("txtusu");
+
+    if (cajaMenu) {
+      cajaMenu.style.display = "none";
+    }
+    if (formLogin) {
+      formLogin.style.display = "block";
+    }
+    if (txtUsu) {
+      txtUsu.value = " ";
+      txtUsu.focus();
+    }
   }
 
   return (
     <>
-      <BrowserRouter >
-        <div id="caja_menu" style={{ border: "8px solid #D2691E"}}>
+      <BrowserRouter>
+        <div id="caja_menu" style={{ border: "8px solid #D2691E" }}>
           <nav
             className="navbar navbar-expand-sm navbar-light"
             style={{ backgroundColor: "#D2691E" }}
           >
-            <div className="container-fluid" style={{ backgroundColor: "#D2691E" }}>
+            <div
+              className="container-fluid"
+              style={{ backgroundColor: "#D2691E" }}
+            >
               <Link className="navbar-brand" to="/home">
                 <img src={iconMenu} style={{ width: "52px" }} />
               </Link>
@@ -57,9 +70,17 @@ const Menu = ({ handleLogout }) => {
                       id="subtitle"
                       aria-current="page"
                       to="/register"
-                      style={{ color: "#FFFFF3", textDecoration: "none", transition: "background-color 0.3s ease-in-out"}}
-                      onMouseEnter={(e) => e.target.style.backgroundColor = "#ba5106"}
-                      onMouseLeave={(e) => e.target.style.backgroundColor = "#D2691E"}
+                      style={{
+                        color: "#FFFFF3",
+                        textDecoration: "none",
+                        transition: "background-color 0.3s ease-in-out",
+                      }}
+                      onMouseEnter={(e) =>
+                        (e.target.style.backgroundColor = "#ba5106")
+                      }
+                      onMouseLeave={(e) =>
+                        (e.target.style.backgroundColor = "#D2691E")
+                      }
                     >
                       Registrar Usuario
                     </Link>
@@ -70,9 +91,17 @@ const Menu = ({ handleLogout }) => {
                       id="subtitle"
                       aria-current="page"
                       to="/list"
-                      style={{ color: "#FFFFF3", textDecoration: "none", transition: "background-color 0.3s ease-in-out"}}
-                      onMouseEnter={(e) => e.target.style.backgroundColor = "#ba5106"}
-                      onMouseLeave={(e) => e.target.style.backgroundColor = "#D2691E"}
+                      style={{
+                        color: "#FFFFF3",
+                        textDecoration: "none",
+                        transition: "background-color 0.3s ease-in-out",
+                      }}
+                      onMouseEnter={(e) =>
+                        (e.target.style.backgroundColor = "#ba5106")
+                      }
+                      onMouseLeave={(e) =>
+                        (e.target.style.backgroundColor = "#D2691E")
+                      }
                     >
                       Usuarios
                     </Link>
@@ -83,9 +112,17 @@ const Menu = ({ handleLogout }) => {
                       id="subtitle"
                       aria-current="page"
                       to="/registerProduct"
-                      style={{ color: "#FFFFF3", textDecoration: "none", transition: "background-color 0.3s ease-in-out"}}
-                      onMouseEnter={(e) => e.target.style.backgroundColor = "#ba5106"}
-                      onMouseLeave={(e) => e.target.style.backgroundColor = "#D2691E"}
+                      style={{
+                        color: "#FFFFF3",
+                        textDecoration: "none",
+                        transition: "background-color 0.3s ease-in-out",
+                      }}
+                      onMouseEnter={(e) =>
+                        (e.target.style.backgroundColor = "#ba5106")
+                      }
+                      onMouseLeave={(e) =>
+                        (e.target.style.backgroundColor = "#D2691E")
+                      }
                     >
                       Registrar Producto
                     </Link>
@@ -96,9 +133,17 @@ const Menu = ({ handleLogout }) => {
                       id="subtitle"
                       aria-current="page"
                       to="/productos"
-                      style={{ color: "#FFFFF3", textDecoration: "none", transition: "background-color 0.3s ease-in-out"}}
-                      onMouseEnter={(e) => e.target.style.backgroundColor = "#ba5106"}
-                      onMouseLeave={(e) => e.target.style.backgroundColor = "#D2691E"}
+                      style={{
+                        color: "#FFFFF3",
+                        textDecoration: "none",
+                        transition: "background-color 0.3s ease-in-out",
+                      }}
+                      onMouseEnter={(e) =>
+                        (e.target.style.backgroundColor = "#ba5106")
+                      }
+                      onMouseLeave={(e) =>
+                        (e.target.style.backgroundColor = "#D2691E")
+                      }
                     >
                       Productos
                     </Link>
@@ -109,9 +154,17 @@ const Menu = ({ handleLogout }) => {
                       id="subtitle"
                       aria-current="page"
                       to="/categorias"
-                      style={{ color: "#FFFFF3", textDecoration: "none", transition: "background-color 0.3s ease-in-out"}}
-                      onMouseEnter={(e) => e.target.style.backgroundColor = "#ba5106"}
-                      onMouseLeave={(e) => e.target.style.backgroundColor = "#D2691E"}
+                      style={{
+                        color: "#FFFFF3",
+                        textDecoration: "none",
+                        transition: "background-color 0.3s ease-in-out",
+                      }}
+                      onMouseEnter={(e) =>
+                        (e.target.style.backgroundColor = "#ba5106")
+                      }
+                      onMouseLeave={(e) =>
+                        (e.target.style.backgroundColor = "#D2691E")
+                      }
                     >
                       Categorias
                     </Link>
@@ -122,65 +175,117 @@ const Menu = ({ handleLogout }) => {
                       className="nav-link active"
                       aria-current="page"
                       to="/cotizaciones"
-                      style={{ color: "#FFFFF3", textDecoration: "none", transition: "background-color 0.3s ease-in-out"}}
-                      onMouseEnter={(e) => e.target.style.backgroundColor = "#ba5106"}
-                      onMouseLeave={(e) => e.target.style.backgroundColor = "#D2691E"}
+                      style={{
+                        color: "#FFFFF3",
+                        textDecoration: "none",
+                        transition: "background-color 0.3s ease-in-out",
+                      }}
+                      onMouseEnter={(e) =>
+                        (e.target.style.backgroundColor = "#ba5106")
+                      }
+                      onMouseLeave={(e) =>
+                        (e.target.style.backgroundColor = "#D2691E")
+                      }
                     >
                       Cotizaciones
                     </Link>
-                  </li >
-                  <li className="nav-item" style={{ backgroundColor: "#D2691E" }}>
+                  </li>
+                  <li
+                    className="nav-item"
+                    style={{ backgroundColor: "#D2691E" }}
+                  >
                     <Link
                       id="subtitle"
                       className="nav-link active"
                       aria-current="page"
                       to="/clientes"
-                      style={{ color: "#FFFFF3", textDecoration: "none", transition: "background-color 0.3s ease-in-out"}}
-                      onMouseEnter={(e) => e.target.style.backgroundColor = "#ba5106"}
-                      onMouseLeave={(e) => e.target.style.backgroundColor = "#D2691E"}
+                      style={{
+                        color: "#FFFFF3",
+                        textDecoration: "none",
+                        transition: "background-color 0.3s ease-in-out",
+                      }}
+                      onMouseEnter={(e) =>
+                        (e.target.style.backgroundColor = "#ba5106")
+                      }
+                      onMouseLeave={(e) =>
+                        (e.target.style.backgroundColor = "#D2691E")
+                      }
                     >
                       Clientes
                     </Link>
-                  </li >
-                  <li className="nav-item" style={{ backgroundColor: "#D2691E" }}>
+                  </li>
+                  <li
+                    className="nav-item"
+                    style={{ backgroundColor: "#D2691E" }}
+                  >
                     <Link
                       id="subtitle"
                       className="nav-link active"
                       aria-current="page"
                       to="/workshops"
-                      style={{ color: "#FFFFF3", textDecoration: "none", transition: "background-color 0.3s ease-in-out"}}
-                      onMouseEnter={(e) => e.target.style.backgroundColor = "#ba5106"}
-                      onMouseLeave={(e) => e.target.style.backgroundColor = "#D2691E"}
+                      style={{
+                        color: "#FFFFF3",
+                        textDecoration: "none",
+                        transition: "background-color 0.3s ease-in-out",
+                      }}
+                      onMouseEnter={(e) =>
+                        (e.target.style.backgroundColor = "#ba5106")
+                      }
+                      onMouseLeave={(e) =>
+                        (e.target.style.backgroundColor = "#D2691E")
+                      }
                     >
                       Registro Talleres
                     </Link>
-                  </li >
-                  <li className="nav-item" style={{ backgroundColor: "#D2691E" }}>
+                  </li>
+                  <li
+                    className="nav-item"
+                    style={{ backgroundColor: "#D2691E" }}
+                  >
                     <Link
                       id="subtitle"
                       className="nav-link active"
                       aria-current="page"
                       to="/AllWorkshops"
-                      style={{ color: "#FFFFF3", textDecoration: "none", transition: "background-color 0.3s ease-in-out"}}
-                      onMouseEnter={(e) => e.target.style.backgroundColor = "#ba5106"}
-                      onMouseLeave={(e) => e.target.style.backgroundColor = "#D2691E"}
+                      style={{
+                        color: "#FFFFF3",
+                        textDecoration: "none",
+                        transition: "background-color 0.3s ease-in-out",
+                      }}
+                      onMouseEnter={(e) =>
+                        (e.target.style.backgroundColor = "#ba5106")
+                      }
+                      onMouseLeave={(e) =>
+                        (e.target.style.backgroundColor = "#D2691E")
+                      }
                     >
                       Talleres
                     </Link>
-                  </li >
+                  </li>
                 </ul>
-                  <span className="navbar-text" style={{ backgroundColor: "#D2691E" }}>
-                    <button
-                      className="btn btn-outline-success"
-                      onClick={LogOut}
-                      style={{ color: "#FFFFF3", textDecoration: "none", transition: "background-color 0.3s ease-in-out"}}
-                      onMouseEnter={(e) => e.target.style.backgroundColor = "##4C463D"}
-                      onMouseLeave={(e) => e.target.style.backgroundColor = "#fa6b05"}
-                      id="subtitle"
-                    >
-                      Cerrar sesión
-                    </button>
-                  </span>
+                <span
+                  className="navbar-text"
+                  style={{ backgroundColor: "#D2691E" }}
+                >
+                  <button
+                    className="btn btn-outline-success"
+                    onClick={LogOut}
+                    style={{
+                      color: "#FFFFF3",
+                      textDecoration: "none",
+                      transition: "background-color 0.3s ease-in-out",
+                    }}
+                    onMouseEnter={(e) =>
+                      (e.target.style.backgroundColor = "##4C463D")
+                    }
+                    onMouseLeave={(e) =>
+                      (e.target.style.backgroundColor = "#fa6b05")
+                    }
+                    id="subtitle"
+                  >
+                    Cerrar sesión
+                  </button>
+                </span>
               </div>
             </div>
           </nav>
@@ -188,7 +293,10 @@ const Menu = ({ handleLogout }) => {
         <Routes>
           <Route path="/register" element={<RegisterUser />}></Route>
           <Route path="/list" element={<UserList />}></Route>
-          <Route path="/registerProduct"element={<RegisterProductAux />}></Route>
+          <Route
+            path="/registerProduct"
+            element={<RegisterProductAux />}
+          ></Route>
           <Route path="/productos" element={<ProductList />}></Route>
           <Route path="/categorias" element={<CategoryList />}></Route>
           <Route path="/cotizaciones" element={<Quotes />}></Route>
